9202addc6fa9bd5e6244d3097d3a4dd9f738d889
nicoladeveloper <ffnicolaf13@gmail.com>
2025-09-15 16:16:19 -0300

    Programa de Xp

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("ola mundão")
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Pergunta o nome do herói
+rl.question("Digite o nome do herói: ", (nomeHeroi) => {
+  rl.question("Digite a quantidade de XP: ", (xpInput) => {
+    let xp = Number(xpInput);
+    let nivel;
+
+    if (xp <= 1000) {
+      nivel = "Ferro";
+    } else if (xp <= 2000) {
+      nivel = "Bronze";
+    } else if (xp <= 5000) {
+      nivel = "Prata";
+    } else if (xp <= 6000) {
+      nivel = "Ouro";
+    } else if (xp <= 8000) {
+      nivel = "Platina Diamante";
+    } else if (xp <= 9000) {
+      nivel = "Ascendente";
+    } else if (xp <= 10000) {
+      nivel = "Imortal";
+    } else {
+      nivel = "Radiante";
+    }
+
+    console.log(`\nO herói de nome ${nomeHeroi} está no nível de ${nivel}!\n`);
+
+    rl.close();
+  });
+});
